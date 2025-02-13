b09b8d4ef87a6e0e0f6e6a95486ef4f4db973943
nsmith167 <n.smith167@gmail.com>
2025-02-12 21:33:35 -0500

    Temp endpoints working

--- src/main/java/nsmith167/homemetrics/temphumidity/controller/TempHumidityController.java
+++ src/main/java/nsmith167/homemetrics/temphumidity/controller/TempHumidityController.java
@@ -4,6 +4,9 @@ import nsmith167.homemetrics.temphumidity.dto.TempHumidityDto;
 import nsmith167.homemetrics.temphumidity.mapper.TempHumidityDtoMapper;
 import nsmith167.homemetrics.temphumidity.model.TempHumidityReading;
 import nsmith167.homemetrics.temphumidity.service.TempHumidityService;
+import nsmith167.homemetrics.weather.dto.WeatherReadingDto;
+import nsmith167.homemetrics.weather.mapper.WeatherReadingDtoMapper;
+import nsmith167.homemetrics.weather.model.WeatherReading;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.List;
 
 @RestController
 @RequestMapping("/temp-humidity")
@@ -33,5 +37,21 @@ public class TempHumidityController {
                 tempHumidityDtoMapper.tempHumidityReadingToDto(reading),
                 HttpStatus.OK
         );
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<TempHumidityDto>> getTempHumidityHistory(
+            @RequestParam("location") String location,
+            @RequestParam(value = "startTime", required = true, defaultValue = "0") long startTimeSeconds,
+            @RequestParam(value = "endTime", required = true, defaultValue = "0") long endTimeSeconds
+    ) {
+        Instant startTime = Instant.ofEpochSecond(startTimeSeconds);
+        Instant endTime = Instant.ofEpochSecond(endTimeSeconds);
+        List<TempHumidityReading> results = tempHumidityService.getTempHumidityHistory(location, startTime, endTime);
+        List<TempHumidityDto> response = results.stream()
+                .map(tempHumidityDtoMapper::tempHumidityReadingToDto)
+                .toList();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- src/main/java/nsmith167/homemetrics/temphumidity/service/TempHumidityService.java
+++ src/main/java/nsmith167/homemetrics/temphumidity/service/TempHumidityService.java
@@ -3,6 +3,8 @@ package nsmith167.homemetrics.temphumidity.service;
 import nsmith167.homemetrics.temphumidity.model.TempHumidityReading;
 import nsmith167.homemetrics.temphumidity.repository.TempHumidityRepository;
 import nsmith167.homemetrics.temphumidity.repository.TempHumidityRepositoryAggregates;
+import nsmith167.homemetrics.weather.model.WeatherReading;
+import nsmith167.homemetrics.weather.repository.WeatherRepositoryAggregates;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
@@ -10,6 +12,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
 
@@ -31,6 +34,15 @@ public class TempHumidityService {
         return tempHumidityRepository.findTempHumidityReadingsByAggregatePipeline(
                 newAggregation(matchZip, filterTimestamp, sortByTimestampDesc, limitOne)
         ).getFirst();
+    }
+
+    public List<TempHumidityReading> getTempHumidityHistory(String location, Instant startTime, Instant endTime) {
+        AggregationOperation matchZip = Aggregation.match(Criteria.where(TempHumidityRepositoryAggregates.DATA_MODEL_LOCATION_PATH).is(location));
+        AggregationOperation sortByTimestampDesc = Aggregation.sort(Sort.Direction.DESC, TempHumidityRepositoryAggregates.DATA_MODEL_TIMESTAMP_PATH);
+        AggregationOperation filterTimestamp = Aggregation.match(Criteria.where(TempHumidityRepositoryAggregates.DATA_MODEL_TIMESTAMP_PATH).lte(endTime).gte(startTime));
+        return tempHumidityRepository.findTempHumidityReadingsByAggregatePipeline(
+                newAggregation(matchZip, sortByTimestampDesc, filterTimestamp)
+        );
     }
 
     public void saveTempHumidityReading(TempHumidityReading reading) {
